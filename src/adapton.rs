c8d155c379d58976ebd065c0572b92319834faf8
Hammer <hammer@cs.umd.edu>
2014-12-23 15:34:59 -0500

    starting out with Rust basics, and Nominal Adapton basics

---
+++ src/adapton.rs
@@ -0,0 +1,35 @@
+mod name {
+    #[deriving(Clone,Show,Hash)]
+    enum Name { 
+        Symbol (Box<String>),
+        ForkL (Box<Name>),
+        ForkR (Box<Name>),
+        Pair (Box<Name>,Box<Name>),
+    }
+    #[allow(dead_code)]
+    pub fn symbol (s:String) -> Name {
+        Name::Symbol(box s)
+    }
+    #[allow(dead_code)]
+    pub fn fork (n:Name) -> (Name,Name) {
+        let m = n.clone () ;
+        (Name::ForkL(box n), Name::ForkR(box m))
+    }    
+    #[allow(dead_code)]
+    pub fn pair (n:Name, m:Name) -> Name {
+        Name::Pair(box n, box m)
+    }
+
+    #[test]
+    fn printstuff () {
+        let n = symbol (format!("one")) ;
+        let m = symbol (format!("two")) ;
+        println!("Adapton: {}, {}", n, m);
+        println!("Adapton: {}, {}", fork(n), m);
+    }
+}
+
+
+fn main () {
+
+}
