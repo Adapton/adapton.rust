3c5d52c5364e5d6253f960c0157be8518e1149d1
Hammer <hammer@cs.umd.edu>
2014-12-23 18:25:13 -0500

    trying cargo test

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ extern crate lazy;
 extern crate memoirs;
 
 pub use lazy::single::Thunk;
-pub use adapton::name::printstuff;
 
+#[allow(dead_code)]
 fn main () {
-    printstuff();
     let x = Thunk::new(move |:| { 666u });
     println!("hello world, the answer is {}.", x.unwrap())
 }
---
+++ tests/name.rs
@@ -0,0 +1,24 @@
+#![feature(globs)]
+extern crate adapton;
+extern crate test;
+
+use adapton::name::*;
+use test::Bencher;
+
+#[test]
+pub fn printstuff () {
+    let n = symbol (format!("one")) ;
+    let m = symbol (format!("two")) ;
+    println!("Adapton: {}, {}", n, m);
+    println!("Adapton: {}, {}", fork(n), m);
+}
+
+#[bench]
+pub fn printstuff_bench (b:&mut Bencher) {
+    b.iter(|| {
+        let n = symbol (format!("one")) ;
+        let m = symbol (format!("two")) ;
+        println!("Adapton: {}, {}", n, m);
+        println!("Adapton: {}, {}", fork(n), m);
+    })
+}
