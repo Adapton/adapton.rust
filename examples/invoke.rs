7966aacf2f697b355ac51634cc07d18a3382d4ea
Hammer <hammer@cs.umd.edu>
2015-01-07 14:28:31 -0500

    documented

--- examples/invoke.rs
+++ examples/invoke.rs
@@ -1,9 +1,11 @@
 use std::thunk::Invoke;
 
+// Consume a closure
 fn foo (f:Box<Invoke<(), int>>) {
     print!("{}", f.invoke(()) );
 }
 
+// Produce a closure
 pub fn main () {    
     let f : Box<Invoke<(),int>> = box move |:()| 1 ;
     foo (f);
