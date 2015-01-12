81727f0102d9070bd75a7e5e81e88354f6476c03
Hammer <hammer@cs.umd.edu>
2015-01-12 17:20:13 -0500

    still confused about typing issues with balfp traits

--- src/balfp.rs
+++ src/balfp.rs
@@ -3,7 +3,7 @@ use std::hash::Hash;
 use std::int::MAX;
 use std::num::Int;
 
-/// Our fixed-point loop needs a way of hashing the "current state" (type `St).
+/// Our fixed-point loop needs a way of hashing the "current state" (type `St`).
 /// The details of what states are and how they relate need not be revealed.
 /// We hide all of these details in a "state system" (type `Sys`).
 /// `next` checks for unstepped states to become the "current state".
@@ -11,13 +11,13 @@ use std::num::Int;
 trait Step {
     type St : Hash ;
     type Sys ;
-    pub fn step (st:Self::St, sys:Self::Sys) -> Self::Sys ;
-    pub fn next (sys:Self::Sys) -> Option<Self::St> ;
+    fn step (st:Self::St, sys:Self::Sys) -> Self::Sys ;
+    fn next (sys:Self::Sys) -> Option<Self::St> ;
 }
 
 trait Fp<S:Step> {
-    pub fn work_tree (left_lev:int, sys:S::Sys, parent_lev:int) -> S::Sys ;
-    pub fn fp ( st:S::Sys ) -> S::Sys ;
+    fn work_tree (left_lev:int, sys:S::Sys, parent_lev:int) -> S::Sys ;
+    fn fp ( st:S::Sys ) -> S::Sys ;
 }
 
 struct FpS;
@@ -25,27 +25,31 @@ struct FpS;
 impl<S:Step> Fp<S> for FpS {
     
     fn work_tree (left_lev:int, sys:S::Sys, parent_lev:int) -> S::Sys {
-        let nxt = Step::next(sys) ;
-        match nxt {
-            None => sys,
-            Some(st) => {
-                let st_lev = Int::trailing_zeros(hash::hash(&st)) as int ;
-                if ( left_lev <= st_lev &&
-                     st_lev   <= parent_lev )
-                {
-                    let sys = Step::step(st, sys) ;
-                    let sys = Fp::work_tree ( -1,     sys, st_lev );
-                    let sys = Fp::work_tree ( st_lev, sys, parent_lev );
-                    sys
-                }
-                else {
-                    sys
-                }
-            }
-        }
+
+        panic!("!");
+        // let nxt = Step::next(sys) ;
+        // match nxt {
+        //     None => sys,
+        //     Some(st) => {
+        //         let st_lev = Int::trailing_zeros(hash::hash(&st)) as int ;
+        //         if ( left_lev <= st_lev &&
+        //              st_lev   <= parent_lev )
+        //         {
+        //             let sys = Step::step(st, sys) ;
+        //             let sys = Fp::work_tree ( -1,     sys, st_lev );
+        //             let sys = Fp::work_tree ( st_lev, sys, parent_lev );
+        //             sys
+        //         }
+        //         else {
+        //             sys
+        //         }
+        //     }
+        // }
+        
     }
     
     fn fp (sys:S::Sys) -> S::Sys {
-        Fp::work_tree (-1, sys, MAX );
+        //Fp::work_tree (-1, sys, MAX );
+        panic!("!");
     }
 }
