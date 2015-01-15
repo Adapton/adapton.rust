ca466fa810b0042fcf6804ea1750c421219bf7e6
Hammer <hammer@cs.umd.edu>
2015-01-15 16:21:40 -0500

    fixed-point computation produces an abstract binary tree

--- src/balfp.rs
+++ src/balfp.rs
@@ -4,6 +4,22 @@ use std::int::MAX;
 use std::num::Int;
 use std::result::Result;
 
+// The idea of a "balanced fixed-point" computation is to do two
+// things simultaneously:
+//
+// (1) compute the fixed-point value of a function by running an
+// (abstracted) iterative algorithm.
+//
+// (2) structure the evaluation of this algorithm as a balanced binary
+// tree of Adapton thunks, where each thunk in the tree stores an
+// intermediate state of the algorithm.
+//
+// The second goal is motivated by Adapton's need for a shallow
+// call-graph.  If one were to write a loop naively, the call-graph
+// may not be shallow.  By using this fixed-point combinator in place
+// of a while loop (or some tail-recursive loop), we can guarantee
+// (probabilistically) that the call-graph is shallow.
+
 trait FpFn {
     // Fixed-point Function.
     //
@@ -15,49 +31,56 @@ trait FpFn {
     ///  - The loop iteration's current state (type `St`)
     //
     ///  - Any "meta-state" needed to explore the space of states
-    ///  (type `Sys`)
+    ///  (type `Sys`).  This meta state may consist of nothing, or it
+    ///  may consist of queues, stacks, and/or sets of
+    ///  previously-visited states.
     //
-    /// Our fixed-point loop needs a way of hashing the "current
-    /// state" (type `St`).  The details of what states are and how
-    /// they relate need not be revealed.  We hide all of these
-    /// details in a "state system" (type `Sys`).  `next` checks for
-    /// unstepped states to become the "current state", or returns Err
-    /// if no unexplored states exist.  `step` updates the system at
-    /// the "current state".
+    /// Our fixed-point loop below needs a way of hashing the "current
+    /// state" (type `St`).  It builds a binary tree of stepped states
+    /// (type `St_stepped`).
     
     type St : Hash ;
+    type St_stepped ;
     type Sys ;
-    fn step (self:& Self, st:Self::St, sys:Self::Sys) -> Self::Sys ;
+    fn step (self:& Self, st:Self::St, sys:Self::Sys) -> (Self::St_stepped, Self::Sys) ;
     fn next (self:& Self, sys:Self::Sys) -> Result<(Self::St, Self::Sys), Self::Sys> ;
 }
 
-trait BuildTree<T> {
-    fn leaf(data:&T) -> Self;
-    fn bin(left:Self,data:&T,right:Self) -> Self;
+trait BinTree<T> {
+    // Our fixed-point loop builds a balanced binary tree of stepped states.
+    type Tree;
+    fn leaf(self:& Self) -> Self::Tree;
+    fn bin(self:& Self, left:Self::Tree, T, right:Self::Tree) -> Self::Tree;
 }
 
-trait FpLp<F:FpFn, BT:BuildTree<F::St>> {
-    fn work_tree (self:&Self, f:&F, left_lev:int, sys:F::Sys, parent_lev:int) -> F::Sys {
+trait FpLp<F:FpFn, BT:BinTree<F::St_stepped>> {
+    // Our fixed-point loop.  It uses a hashing and probabilistic
+    // reasoning to build a binary tree of stepped states.
+    fn work_tree (self:&Self, f:&F, bt:&BT,
+                  left:BT::Tree, left_lev:int,
+                  sys:F::Sys, parent_lev:int) -> (F::Sys, BT::Tree)
+    {
         let nxt = f.next(sys) ;
         match nxt {
-            Err(sys) => sys,
+            Err(sys) => (sys, left),
             Ok((st,sys)) => {
                 let st_lev = Int::trailing_zeros(hash::hash(&st)) as int ;
-                if ( left_lev <= st_lev &&
-                     st_lev   <= parent_lev )
+                if left_lev <= st_lev &&
+                    st_lev  <= parent_lev
                 {
-                    let sys : F::Sys = f.step(st, sys) ;
-                    let sys : F::Sys = self.work_tree (f, -1,     sys, st_lev );
-                    let sys : F::Sys = self.work_tree (f, st_lev, sys, parent_lev );
-                    sys
+                    let (st, sys) = f.step(st, sys) ;
+                    let (sys,right) = self.work_tree (f, bt, bt.leaf(), -1, sys, st_lev );
+                    let tree = bt.bin(left,st,right) ;
+                    let (sys,tree) = self.work_tree (f, bt, tree, st_lev, sys, parent_lev );
+                    (sys,tree)
                 }
                 else {
-                    sys
+                    (sys,left)
                 }
             }
         }    
     }    
-    fn compute (self:&Self, f:&F, sys:F::Sys) -> F::Sys {
-        self.work_tree (f, -1, sys, MAX )
+    fn compute (self:&Self, f:&F, bt:&BT, sys:F::Sys) -> (F::Sys,BT::Tree) {
+        self.work_tree (f, bt, bt.leaf(), -1, sys, MAX )
     }
 }
