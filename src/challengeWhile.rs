0078c923c47f8c942363562730fd0ae423ba77a6
Hammer <hammer@cs.umd.edu>
2015-01-24 12:29:33 -0500

    more challenge problem; sketching Heap trait

--- src/challengeWhile.rs
+++ src/challengeWhile.rs
@@ -2,6 +2,7 @@ use name::*;
 use art::*;
 use std::hash;
 use std::hash::Hash;
+use std::result::Result;
 
 //
 // Cmd C ::= Skip
@@ -30,6 +31,7 @@ pub enum Cmd<'x> {
 }
 pub type CmdB<'x> = Box<Cmd<'x>>;
 
+/// Expressions for the while language
 #[derive(Show,Hash,PartialEq,Eq)]
 pub enum Exp<'x> {
     Var(Var<'x>),
@@ -38,7 +40,25 @@ pub enum Exp<'x> {
 }
 pub type ExpB<'x> = Box<Exp<'x>>;
 
+/// Variables for the while language
 pub type Var<'x> = String;
+
+pub trait Heap<T> {
+    fn empty () -> Self ;
+    fn update (Self, Hash, T) -> Self ;
+    fn select (Self, Hash) -> Result<(Self,T), Self> ;
+}
+
+pub enum Err {
+    VarNotDef(String)
+}
+
+pub fn eval_exp<'x,T:'x> (exp:Exp<'x>, heap:Box<Heap<int>>)
+                          -> Result<(Box<Heap<int>>,int), (Box<Heap<int>>,Err)> {
+    panic!("not impl")
+}
+   
+
 
 #[test]
 pub fn doit () {
