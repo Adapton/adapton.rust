8f9a039b2ba1b206e40f1cc0636264d3963a0e40
Hammer <hammer@cs.umd.edu>
2015-02-01 17:43:24 -0500

    implemented name-processing operations

--- examples/obmt.rs
+++ examples/obmt.rs
@@ -1,13 +1,16 @@
-use std::hash::Hash;
+use std::hash::{hash,Hash};
 use std::collections::HashMap;
 use std::thunk::Invoke;
+use std::mem::replace;
+use std::sync::Arc;
 
 // "One big memo table" design
 
 pub trait Adapton {
+    fn new () -> Self ;
     fn name_of_string (self:&mut Self, String) -> Name ;
     fn name_of_u64 (self:&mut Self, u64) -> Name ;
-    fn name_pair (self: &Self, Name, Name) -> Name ;        
+    fn name_pair (self: &Self, Name, Name) -> Name ;
     fn name_fork (self:&mut Self, Name) -> (Name, Name) ;
 
     fn cell<T:Eq+Hash> (self:&mut Self, ArtId, T) -> Art<T> ;
@@ -18,25 +21,32 @@ pub trait Adapton {
     fn force<T:Eq+Hash> (self:&mut Self, Art<T>) -> T ;
 }
 
+#[derive(Hash,Show,PartialEq,Eq)]
 pub enum Lineage {
     Unknown,
-    Pair(Box<Lineage>,Box<Lineage>),
-    ForkL(Box<Lineage>),
-    ForkR(Box<Lineage>),
+    Root,
+    Taken,
+    Pair(Arc<Lineage>,Arc<Lineage>),
+    ForkL(Arc<Lineage>),
+    ForkR(Arc<Lineage>),
     String(String),
     U64(u64),
+    Arc(Arc<Lineage>),
 }
 
+#[derive(Hash,Show,PartialEq,Eq)]
 pub struct Name {
     hash : u64,
-    lineage : Lineage,
+    lineage : Arc<Lineage>,
 }
 
+#[derive(Hash,Show,PartialEq,Eq)]
 pub enum Art<T> {
     Hash(u64),
     Box(Box<T>),
 }
 
+#[derive(Hash,Show,PartialEq,Eq)]
 pub enum ArtId {
     None,
     Structural,
@@ -44,18 +54,36 @@ pub enum ArtId {
 }
 
 pub struct AdaptonState {
-    mtbl : HashMap<Name, *mut ()>,
+    memo_table : HashMap<ArtId, *mut ()>, // memo table
+    curr_nmspc : Name, // current_namespace
+    curr_artnm : Name, // current articulation point
 }
 
 impl Adapton for AdaptonState {
+    fn new () -> AdaptonState {
+        let root = Arc::new(Lineage::Root);
+        let root1 = Name{hash:0, lineage:root.clone()};
+        let root2 = Name{hash:0, lineage:root};
+        AdaptonState {
+            memo_table : HashMap::new (),
+            curr_nmspc : root1,
+            curr_artnm : root2,
+        }
+    }
     fn name_of_string (self:&mut AdaptonState, sym:String) -> Name {
-        panic!("")            
+        let h = hash::<_>(&sym) ;
+        let s = Lineage::String(sym) ;
+        Name{ hash:h, lineage:Arc::new(s) }
     }
     fn name_of_u64 (self:&mut AdaptonState, sym:u64) -> Name {
-        panic!("")            
+        let h = hash::<_>(&sym) ;
+        let s = Lineage::U64(sym) ;
+        Name{ hash:h, lineage:Arc::new(s) }
     }
     fn name_pair (self: &AdaptonState, fst: Name, snd: Name) -> Name {
-        panic!("")
+        let h = hash::<_>( &(&fst,&snd) ) ;
+        let p = Lineage::Pair(fst.lineage, snd.lineage) ;
+        Name{ hash:h, lineage:Arc::new(p) }
     }
     fn name_fork (self:&mut AdaptonState, nm:Name) -> (Name, Name) {
         panic!("")
