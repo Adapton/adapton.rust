54863f0fe6102c0faa523b34838769b93686bddb
Hammer <hammer@cs.umd.edu>
2015-02-02 15:09:28 -0500

    implemented set and force for mutable cells; still need dirtying traversal; thinking about thunks next

--- examples/obmt.rs
+++ examples/obmt.rs
@@ -1,9 +1,9 @@
+use std::fmt::Show;
 use std::hash::{hash,Hash};
 use std::collections::HashMap;
 use std::thunk::Invoke;
 use std::mem::replace;
 //use std::sync::Arc;
-use std::cell::RefCell;
 use std::rc::Rc;
 
 // "One big memo table" design
@@ -20,17 +20,17 @@ pub trait Adapton {
     fn ns<T,F> (self: &mut Self, Name, body:F) -> T
         where F:FnOnce(&mut Self) -> T ;
 
-    fn put<T:Eq> (self:&mut Self, T) -> Art<T> ;
+    fn put<T:Eq+Clone+Show> (self:&mut Self, T) -> Art<T> ;
 
     // Mutable cells
-    fn cell<T:Eq> (self:&mut Self, ArtId, T) -> MutArt<T> ;
-    fn set<T:Eq> (self:&mut Self, MutArt<T>) ;
+    fn cell<T:Eq+Clone+Show> (self:&mut Self, ArtId, T) -> MutArt<T> ;
+    fn set<T:Eq+Clone+Show> (self:&mut Self, MutArt<T>, T) ;
 
-    fn thunk<Arg:Eq+Hash,T:Eq>
+    fn thunk<Arg:Eq+Hash+Clone+Show,T:Eq+Clone+Show>
         (self:&mut Self, id:ArtId,
          fn_body:Box<Invoke<Arg, T> + 'static>, arg:Arg) -> Art<T> ;
     
-    fn force<T:Eq> (self:&mut Self, Art<T>) -> T ;
+    fn force<T:Eq+Clone+Show> (self:&mut Self, Art<T>) -> T ;
 }
 
 #[derive(Hash,Show,PartialEq,Eq)]
@@ -82,42 +82,43 @@ pub enum MutArt<T> {
     MutArt(Art<T>)
 }
 
+#[derive(Show)]
 enum Node<T> {
     Pure(PureNode<T>),
     Mut(MutNode<T>),
     Compute(ComputeNode<T>),
 }
 
+#[derive(Show)]
 struct PureNode<T> {
     loc : Rc<Loc>,
     val : T,
 }
 
+#[derive(Show)]
 struct MutNode<T> {
     loc : Rc<Loc>,
     dem_precs : Vec<DemPrec>,
-    val : RefCell<T>,
+    val : T,
 }
 
-trait Body<T> {
-    fn run (self:&Self, st:&mut AdaptonState) -> T ;
-}
-
+#[derive(Show)]
 struct ComputeNode<T> {
     loc : Rc<Loc>,
     dem_precs : Vec<DemPrec>,
     dem_succs : Vec<DemSucc>,
-    body : Box<Body<T> + 'static>, // Trait in Type: Need lifetime.
-    val  : RefCell<Option<T>>,
+    val  : Option<T>,
 }
 
+#[derive(Show)]
 struct DemPrec {
     loc : Rc<Loc>,
 }
 
+#[derive(Show)]
 struct DemSucc {
     loc : Rc<Loc>,
-    dirty : RefCell<bool>
+    dirty : bool
 }
 
 pub struct AdaptonState {
@@ -170,22 +171,84 @@ impl Adapton for AdaptonState {
     fn put<T:Eq> (self:&mut AdaptonState, x:T) -> Art<T> {
         Art::Box(box x)
     }
-    fn cell<T:Eq> (self:&mut AdaptonState, id:ArtId, x:T) -> MutArt<T> {        
-        panic!("")
+    fn cell<T:Eq+Clone+Show> (self:&mut AdaptonState, id:ArtId, x:T) -> MutArt<T> {
+        match id {
+            ArtId::None => panic!(""),
+            ArtId::Structural(hash) => {
+                let loc = Loc{hash:hash,
+                              name:Rc::new(Name{hash:hash,
+                                                lineage:Rc::new(Lineage::Structural)}),
+                              path:self.curr_path.clone()} ;
+                MutArt::MutArt(Art::Loc(Rc::new(loc)))
+            }
+            ArtId::Nominal(nm) => {
+                let loc = Loc{hash:nm.hash,
+                              name:Rc::new(nm),
+                              path:self.curr_path.clone()} ;
+                MutArt::MutArt(Art::Loc(Rc::new(loc)))
+            }
+        }
     }
-    fn set<T:Eq> (self:&mut Self, cell:MutArt<T>) {
-        panic!("")
+    fn set<T:Eq+Clone+Show> (self:&mut Self, cell:MutArt<T>, val:T) {
+        match cell {
+            MutArt::MutArt(Art::Box(b)) => {
+                panic!("cannot set pure value");
+            }
+            MutArt::MutArt(Art::Loc(loc)) => {
+                let node = self.memo_table.get(&loc) ;
+                match node {
+                    None => panic!("dangling pointer: {}", loc),
+                    Some(ptr) => {
+                        let node : &mut MutNode<T> = unsafe {
+                            let node = std::mem::transmute::<*mut (), *mut Node<T>>(*ptr) ;
+                            match *node {
+                                Node::Mut(ref mut nd) => nd,
+                                ref nd => panic!("impossible: {}", nd)
+                            }} ;
+                        if (node.val == val) {
+                            // Nothing.
+                        }
+                        else {
+                            node.val = val;
+                            // TODO: Dirty traversal. Notify/mark demand precs.
+                        }
+                    }
+                }
+            }
+        }
     }
-    fn thunk<Arg:Eq+Hash,T:Eq>
+    fn thunk<Arg:Eq+Hash+Clone+Show,T:Eq+Clone+Show>
         (self:&mut AdaptonState,
          id:ArtId, fn_body:Box<Invoke<Arg,T>+'static>, arg:Arg) -> Art<T>
     {
         panic!("")
     }        
-    fn force<T:Eq> (self:&mut AdaptonState, art:Art<T>) -> T {
+    fn force<T:Eq+Clone+Show> (self:&mut AdaptonState, art:Art<T>) -> T {
         match art {
             Art::Box(b) => *b,
-            Art::Loc(l) => panic!(""),
+            Art::Loc(loc) => {
+                let node = self.memo_table.get(&loc) ;
+                match node {
+                    None => panic!("dangling pointer: {}", loc),
+                    Some(ptr) => {
+                        let node : &mut Node<T> = unsafe {
+                            let node = std::mem::transmute::<*mut (), *mut Node<T>>(*ptr) ;
+                            &mut ( *node ) } ;
+                        match *node {
+                            Node::Pure(ref mut nd) => {
+                                nd.val.clone()
+                            }
+                            Node::Mut(ref mut nd) => {
+                                // TODO: Record dependency edge
+                                nd.val.clone()
+                            }
+                            Node::Compute(ref mut nd) => {
+                                panic!("TODO")
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
