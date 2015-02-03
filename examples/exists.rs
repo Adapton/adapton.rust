ac959c173dcc2b8ed37d14c55b3a3b7dd8f32690
Hammer <hammer@cs.umd.edu>
2015-02-03 15:38:43 -0500

    filed issue with rustc

--- examples/exists.rs
+++ examples/exists.rs
@@ -1,27 +1,6 @@
+// Filed issue:
+// https://github.com/rust-lang/rust/issues/21909
 
-struct AdaptonState {
-    foo : bool,
-}
-
-trait Computer<Res> {
-    type Arg;
-    fn compute(self:&Self, st:&mut AdaptonState, arg:Self::Arg) -> Res;
-    //fn size_hint(&self) -> (usize, Option<usize>) { ... }
-}
-
-struct ComputeNode<Res,Comp:Computer<Res>> {
-    arg : Comp::Arg,
-    res : Option<Res>,
-    comp : Box<Comp>,
-}
-
-trait Nd where Self::Comp : Computer<Self::Res> {
-    type Res;
-    type Comp;
-    fn get_node<'x>(self:&'x mut Self) -> &'x mut ComputeNode<Self::Res,Self::Comp>;
-}
-
-
-fn main () {
-
-}
+trait A<X> { }
+trait B { type X; type Y : A<Self::X>; }
+fn main () { }
