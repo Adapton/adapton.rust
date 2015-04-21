5756dac27a3f2bb4ac617c0a214d3620ffa9ea41
Hammer <hammer@cs.umd.edu>
2015-04-21 09:57:38 -0400

    associated types example compiles

--- examples/assoctypes.rs
+++ examples/assoctypes.rs
@@ -1,4 +1,8 @@
 // Traits can have associated types:
+#![feature(collections)]
+
+use std::fmt::Debug;
+//use std::string;
 
 // I thought that associated types could be used to encode existential
 // types, as attempted below.  The trait T has associated type t.  I
@@ -16,33 +20,37 @@
 // examples/assoctypes.rs:24:23: 24:24 error: the value of the associated type `t` (from the trait `T`) must be specified [E0191]
 //    examples/assoctypes.rs:24     two : (Box<T>,Box<T>)
     
-trait T {
+trait T : Debug {
     type t;
     fn get (self:&Self) -> Self::t ;
-    fn foo ( Self::t ) -> String ;
+    fn foo (self:&Self, Self::t ) -> String ;
 }
 
+#[derive(Debug)]
 pub enum Foo { Foo }
+
+#[derive(Debug)]
 pub enum Bar { Bar }
 
 impl T for Foo {
     type t = String;
-    fn get (self) { "foo" }
-    fn foo (s:Self::t) { s }
+    fn get (self:&Self) -> String { String::from_str("foo") }
+    fn foo (self:&Self, s:Self::t) -> String { s }
 }
 
 impl T for Bar {
     type t = u32;
-    fn get (self) { 0 }
-    fn foo (s:Self::t) { "bar" }
+    fn get (self:&Self) -> u32 { 0 }
+    fn foo (self:&Self, _s:Self::t) -> String { String::from_str("bar") }
 }
 
+#[derive(Debug)]
 struct Packed {
-    two : (Box<T>,Box<T>)
+    two : (Box<T<t=String>>,Box<T<t=u32>>)
 }
 
 pub fn main () {
     let a = Packed { two : (Box::new(Foo::Foo),
                             Box::new(Bar::Bar)) } ;
-    ()
+     println!("{:?}", a);
 }
