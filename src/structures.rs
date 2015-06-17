e4eddc36ae5fcccc8b87367e37d3e9f148715034
Hammer <hammer@cs.umd.edu>
2015-06-16 20:27:40 -0400

    wrote most of generic tree_of_list conversion

--- src/structures.rs
+++ src/structures.rs
@@ -6,12 +6,11 @@ use std::rc::Rc;
 use adapton_syntax::* ;
 use adapton_sigs::* ;
 
-pub trait ListT<A:Adapton> {
+pub trait ListT<A:Adapton,Hd> {
     type List ;
-    type Hd ;
     
     fn nil  (&mut A) -> Self::List ;
-    fn cons (&mut A, Self::Hd, Rc<Self::List>) -> Self::List ;
+    fn cons (&mut A, Hd, Rc<Self::List>) -> Self::List ;
     
     // requisite "adaptonic" constructors: `name` and `art`:
     fn name (&mut A, A::Name, Rc<Self::List>) -> Self::List ;
@@ -19,11 +18,11 @@ pub trait ListT<A:Adapton> {
 
     fn elim<Res,Nil,Cons,Name> (&mut A, &Self::List, Nil, Cons, Name) -> Res
         where Nil:FnOnce(&mut A) -> Res
-        ,    Cons:FnOnce(&mut A, &Self::Hd, &Rc<Self::List>) -> Res
+        ,    Cons:FnOnce(&mut A, &Hd, &Rc<Self::List>) -> Res
         ,    Name:FnOnce(&mut A, &A::Name, &Rc<Self::List>) -> Res ;
         
     fn fold<Res,Cons> (self:&Self, &mut A, &Self::List, Res, Cons) -> Res
-        where Cons:Fn(&mut A, Res, &Self::Hd) -> Res ;
+        where Cons:Fn(&mut A, Res, &Hd) -> Res ;
 
     // TODO: Add derived operations (max, min, sum, etc.)
 }
@@ -32,26 +31,24 @@ pub trait ListT<A:Adapton> {
 //  - Should `Name`s always be passed by reference?
 //  - Do these Fn argss for fold need to be passed in `Rc<Box<_>>`s ?
 
-pub trait TreeT<A:Adapton> {
+pub trait TreeT<A:Adapton,Leaf,Bin:Hash> {
     type Tree ;
-    type Leaf ;
-    type Bin : Hash ;
 
     fn nil  (&mut A) -> Self::Tree ;
-    fn leaf (&mut A, Self::Leaf) -> Self::Tree ;
-    fn bin  (&mut A, Self::Bin, Self::Tree, Self::Tree) -> Self::Tree ;
+    fn leaf (&mut A, Leaf) -> Self::Tree ;
+    fn bin  (&mut A, Bin, Rc<Self::Tree>, Rc<Self::Tree>) -> Self::Tree ;
 
     // requisite "adaptonic" constructors: `name` and `art`:
-    fn name (&mut A, A::Name, Self::Tree, Self::Tree) -> Self::Tree ;
+    fn name (&mut A, A::Name, Rc<Self::Tree>, Rc<Self::Tree>) -> Self::Tree ;
     fn art  (&mut A, Art<Self::Tree,A::Loc>) -> Self::Tree ;
         
-    fn fold<Res,Leaf,Bin> (&mut A, Self::Tree, Res, Leaf, Bin) -> Res
-        where Leaf:Fn(&mut A, Res, Self::Leaf) -> Res
-        ,      Bin:Fn(&mut A, Res, Self::Bin ) -> Res ;
+    fn fold<Res,LeafC,BinC> (&mut A, Self::Tree, Res, LeafC, BinC) -> Res
+        where LeafC:Fn(&mut A, Res, Leaf) -> Res
+        ,      BinC:Fn(&mut A, Res, Bin ) -> Res ;
 
-    fn fold2<Arg,Res,Leaf,Bin> (&mut A, Self::Tree, Arg, Leaf, Bin) -> Res
-        where Leaf:Fn(&mut A, Arg, Self::Leaf) -> Res
-        ,      Bin:Fn(&mut A, Arg, Self::Bin, Res, Res ) -> Res ;
+    fn fold2<Arg,Res,LeafC,BinC> (&mut A, Self::Tree, Arg, LeafC, BinC) -> Res
+        where LeafC:Fn(&mut A, Arg, Leaf) -> Res
+        ,      BinC:Fn(&mut A, Arg, Bin, Res, Res ) -> Res ;
 }
 
 
@@ -63,8 +60,7 @@ enum List<A:Adapton,Hd> {
     Art(Art<List<A,Hd>, A::Loc>),
 }
 
-impl<A:Adapton+Debug+PartialEq+Eq+Hash,Hd:Debug+PartialEq+Eq+Hash> ListT<A> for List<A,Hd> {
-    type Hd = Hd;
+impl<A:Adapton+Debug+PartialEq+Eq+Hash,Hd:Debug+PartialEq+Eq+Hash> ListT<A,Hd> for List<A,Hd> {
     type List = List<A,Hd>;
 
     fn nil  (_:&mut A)                                 -> Self::List { List::Nil }
@@ -74,13 +70,13 @@ impl<A:Adapton+Debug+PartialEq+Eq+Hash,Hd:Debug+PartialEq+Eq+Hash> ListT<A> for 
 
     fn elim<Res,Nil,Cons,Name> (st:&mut A, list:&Self::List, nilf:Nil, consf:Cons, namef:Name) -> Res
         where Nil:FnOnce(&mut A) -> Res
-        ,    Cons:FnOnce(&mut A, &Self::Hd, &Rc<Self::List>) -> Res
+        ,    Cons:FnOnce(&mut A, &Hd, &Rc<Self::List>) -> Res
         ,    Name:FnOnce(&mut A, &A::Name, &Rc<Self::List>) -> Res
     {
         panic!("")
     }
     fn fold<Res,Cons> (self:&Self, st:&mut A, list:&Self::List, res:Res, consf:Cons) -> Res
-        where Cons:Fn(&mut A, Res, &Self::Hd) -> Res
+        where Cons:Fn(&mut A, Res, &Hd) -> Res
     {
         match *list {
             List::Nil => res,
@@ -98,35 +94,34 @@ impl<A:Adapton+Debug+PartialEq+Eq+Hash,Hd:Debug+PartialEq+Eq+Hash> ListT<A> for 
 }
 
 pub fn tree_of_list_rec <A:Adapton, X:Hash+Clone, T:TreeT<A,X,X>, L:ListT<A,X>>
-    (st:&mut A, list:&L::List, left_tree:T::Tree)
-     -> (T::Tree, L::List)
-    where X=L::Hd, X=T::Bin
+    (st:&mut A, list:&Rc<L::List>, left_tree:&Rc<T::Tree>)
+     -> (Rc<T::Tree>, Rc<L::List>)
 {
     L::elim (
         st, &list,
-        /* Nil */  |st| ( T::nil(st), L::nil(st) ),
+        /* Nil */  |st| ( Rc::new(T::nil(st)), Rc::new(L::nil(st)) ),
         /* Cons */ |st, hd, rest| {
             if my_hash( hd ) == 0 {
-                let nil = T::nil(st) ;
-                let (right_tree, rest) = tree_of_list_rec::<A,X,T,L> ( st, rest, nil ) ;
-                let tree = T::bin ( st, hd, left_tree, right_tree ) ;
-                tree_of_list_rec::<A,X,T,L> ( st, &rest, tree )
+                let nil = Rc::new(T::nil(st)) ;
+                let (right_tree, rest) = tree_of_list_rec::<A,X,T,L> ( st, rest, &nil ) ;
+                let tree = Rc::new(T::bin ( st, hd.clone(), left_tree.clone(), right_tree )) ;
+                tree_of_list_rec::<A,X,T,L> ( st, &rest, &tree )
             }
             else {
-                let c = L::cons(st, hd.clone(), rest.clone()) ;
-                (left_tree, c)
+                let c = Rc::new(L::cons(st, hd.clone(), rest.clone())) ;
+                (left_tree.clone(), c)
             }},
         /* Name */ |st, nm, rest| {
             if my_hash( nm ) == 0 {
-                let nil = T::nil(st) ;
-                let (right_tree, rest) = tree_of_list_rec::<A,X,T,L> ( st, rest, nil ) ;
+                let nil = Rc::new(T::nil(st)) ;
+                let (right_tree, rest) = tree_of_list_rec::<A,X,T,L> ( st, rest, &nil ) ;
                 // TODO: Place left_ and right_ trees into articulations, named by name.
-                let tree = T::name ( st, nm.clone(), left_tree, right_tree ) ;
-                tree_of_list_rec::<A,X,T,L> ( st, &rest, tree )
+                let tree = Rc::new(T::name( st, nm.clone(), left_tree.clone(), right_tree )) ;
+                tree_of_list_rec::<A,X,T,L> ( st, &rest, &tree )
             }
             else {
-                let c = L::name(st, nm.clone(), rest.clone()) ;
-                (left_tree, c)
+                let c = Rc::new(L::name(st, nm.clone(), rest.clone())) ;
+                (left_tree.clone(), c)
             }}
         )
 }
