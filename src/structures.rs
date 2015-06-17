53f28152bb2644b5c2a61d67b10f6b131161b27d
Hammer <hammer@cs.umd.edu>
2015-06-16 20:39:24 -0400

    wrote generic list elim

--- src/structures.rs
+++ src/structures.rs
@@ -16,7 +16,7 @@ pub trait ListT<A:Adapton,Hd> {
     fn name (&mut A, A::Name, Rc<Self::List>) -> Self::List ;
     fn art  (&mut A, Art<Self::List,A::Loc>) -> Self::List ;
 
-    fn elim<Res,Nil,Cons,Name> (&mut A, &Self::List, Nil, Cons, Name) -> Res
+    fn elim<Res,Nil,Cons,Name> (self:&Self, &mut A, &Self::List, Nil, Cons, Name) -> Res
         where Nil:FnOnce(&mut A) -> Res
         ,    Cons:FnOnce(&mut A, &Hd, &Rc<Self::List>) -> Res
         ,    Name:FnOnce(&mut A, &A::Name, &Rc<Self::List>) -> Res ;
@@ -68,12 +68,20 @@ impl<A:Adapton+Debug+PartialEq+Eq+Hash,Hd:Debug+PartialEq+Eq+Hash> ListT<A,Hd> f
     fn name (_:&mut A, nm:A::Name, tl:Rc<Self::List>)  -> Self::List { List::Name(nm, tl) }
     fn art  (_:&mut A, art:Art<List<A,Hd>,A::Loc>)     -> Self::List { List::Art(art) }
 
-    fn elim<Res,Nil,Cons,Name> (st:&mut A, list:&Self::List, nilf:Nil, consf:Cons, namef:Name) -> Res
+    fn elim<Res,Nil,Cons,Name> (self:&Self, st:&mut A, list:&Self::List, nilf:Nil, consf:Cons, namef:Name) -> Res
         where Nil:FnOnce(&mut A) -> Res
         ,    Cons:FnOnce(&mut A, &Hd, &Rc<Self::List>) -> Res
         ,    Name:FnOnce(&mut A, &A::Name, &Rc<Self::List>) -> Res
     {
-        panic!("")
+        match *list {
+            List::Nil => nilf(st),
+            List::Cons(ref hd, ref tl) => consf(st, hd, tl),
+            List::Name(ref nm, ref tl) => namef(st, nm, tl),
+            List::Art(ref art) => {
+                let list = st.force(art);
+                self.elim(st, &*list, nilf, consf, namef)
+            }
+        }
     }
     fn fold<Res,Cons> (self:&Self, st:&mut A, list:&Self::List, res:Res, consf:Cons) -> Res
         where Cons:Fn(&mut A, Res, &Hd) -> Res
@@ -94,34 +102,35 @@ impl<A:Adapton+Debug+PartialEq+Eq+Hash,Hd:Debug+PartialEq+Eq+Hash> ListT<A,Hd> f
 }
 
 pub fn tree_of_list_rec <A:Adapton, X:Hash+Clone, T:TreeT<A,X,X>, L:ListT<A,X>>
-    (st:&mut A, list:&Rc<L::List>, left_tree:&Rc<T::Tree>)
+    (l:&L, st:&mut A, list:&Rc<L::List>, left_tree:&Rc<T::Tree>)
      -> (Rc<T::Tree>, Rc<L::List>)
 {
-    L::elim (
+    l.elim (
         st, &list,
         /* Nil */  |st| ( Rc::new(T::nil(st)), Rc::new(L::nil(st)) ),
         /* Cons */ |st, hd, rest| {
             if my_hash( hd ) == 0 {
                 let nil = Rc::new(T::nil(st)) ;
-                let (right_tree, rest) = tree_of_list_rec::<A,X,T,L> ( st, rest, &nil ) ;
+                let (right_tree, rest) = tree_of_list_rec::<A,X,T,L> ( l, st, rest, &nil ) ;
                 let tree = Rc::new(T::bin ( st, hd.clone(), left_tree.clone(), right_tree )) ;
-                tree_of_list_rec::<A,X,T,L> ( st, &rest, &tree )
+                tree_of_list_rec::<A,X,T,L> ( l, st, &rest, &tree )
             }
             else {
-                let c = Rc::new(L::cons(st, hd.clone(), rest.clone())) ;
-                (left_tree.clone(), c)
+                let rest = Rc::new(L::cons(st, hd.clone(), rest.clone())) ;
+                (left_tree.clone(), rest)
             }},
         /* Name */ |st, nm, rest| {
             if my_hash( nm ) == 0 {
                 let nil = Rc::new(T::nil(st)) ;
-                let (right_tree, rest) = tree_of_list_rec::<A,X,T,L> ( st, rest, &nil ) ;
+                let (right_tree, rest) = tree_of_list_rec::<A,X,T,L> ( l, st, rest, &nil ) ;
                 // TODO: Place left_ and right_ trees into articulations, named by name.
+                // TODO: Memoize the recursive calls to tree_of_list_rec.
                 let tree = Rc::new(T::name( st, nm.clone(), left_tree.clone(), right_tree )) ;
-                tree_of_list_rec::<A,X,T,L> ( st, &rest, &tree )
+                tree_of_list_rec::<A,X,T,L> ( l, st, &rest, &tree )
             }
             else {
-                let c = Rc::new(L::name(st, nm.clone(), rest.clone())) ;
-                (left_tree.clone(), c)
+                let rest = Rc::new(L::name(st, nm.clone(), rest.clone())) ;
+                (left_tree.clone(), rest)
             }}
         )
 }
