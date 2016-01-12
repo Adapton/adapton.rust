8fc388f794a307c8114b9567371bc3798a8de39d
Matthew Hammer <matthew.hammer@colorado.edu>
2016-01-12 15:30:23 -0700

    experiment for stack size

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # 10*1024*1024 = 10 MB
-export RUST_MIN_STACK=10485760
+# export RUST_MIN_STACK=10485760
 cargo build --verbose
 cargo test --verbose -- --nocapture
 cargo bench --verbose
