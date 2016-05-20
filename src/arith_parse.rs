094e3ab4032d2646a012f0d1670480e011860706
Monal Narasimhamurthy <monal.narasimhamurthy@colorado.edu>
2016-05-20 11:28:44 -0600

    Add runtime harness func, test

--- src/arith_parse.rs
+++ src/arith_parse.rs
@@ -1,8 +1,13 @@
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::rc::Rc;
+use std::path::Path;
 
 use collections::*;
+use engine::*;
+
+extern crate time;
+extern crate csv;
 
 #[derive(Clone,Copy,Hash,Eq,PartialEq,Debug)]
 pub enum Op { Plus, Minus, Times, Divide, ParenOpen, ParenClose }
@@ -136,7 +141,6 @@ pub fn tok_of_char(input: Tree<char>) -> Tree<Tok> {
 
   let (digits, output): (List<char>, List<Tok>)= transfer_num(digits, output);
   assert!(List::is_empty(&digits));
-  println!("Tree<Tok> as list {:?}", output);
   let out  : Tree<Tok> = tree_of_list(Dir2::Right, output);
   (out)
 }
@@ -347,6 +351,75 @@ fn test_tok_of_char() {
    let ans = evaluate_postfix(tree);
    println!("Ans {:?}", ans)
 
+}
+
+pub fn runtime_harness(max_len: isize) -> Vec<(isize, u64)> {
+
+  let mut runtimes: Vec<(isize, u64)> = vec![];
+  //fn construct_exp(len: isize) -> Vec<NameElse<char>>{
+  //  let cap = len as _ ;
+  //  let mut input = Vec::with_capacity(cap);
+  //  for _ in 0..len {
+  //    input.push(NameElse::Else('1'));
+  //    input.push(NameElse::Else('+'));
+  //  }
+  //  input.push(NameElse::Else('1'));
+  //  input
+  //};
+
+  fn push_char(name_step:usize, name:usize, ch: char, l:List<char>) -> List<char>{
+    let l = if name % name_step == 0 {
+      let l = <List<char> as ListIntro<char>>::art(cell(name_of_usize(name), l));
+      let l = <List<char> as ListIntro<char>>::name(name_of_usize(name), l);
+      l
+    } else { l } ;
+    let l = <List<char> as ListIntro<char>>::cons(ch, l);
+    l
+  };
+
+  fn construct_input(len: isize) -> List<char> {
+    let input: List<char> = List::Nil;
+    for _ in 0..len {
+      let input = push(input.clone(), '1');
+      let input = push(input.clone(), '+');
+    }
+    let input = push(input.clone(), '1');
+    input
+  };
+
+  fn doit(input: List<char>) -> isize {
+    //let list : List<char>  = list_of_vec(&input);
+    let tree : Tree<char> = tree_of_list(Dir2::Left, input);
+    let tokenized_input : Tree<Tok> = tok_of_char(tree);
+    let postfix : List<Tok> = postfix_of_infix(tokenized_input);
+    let postfix_tree : Tree<Tok> = tree_of_list(Dir2::Right, postfix);	
+    evaluate_postfix(postfix_tree)
+  };
+
+  fn csv_of_runtimes(runtimes: Vec<(isize, u64)>) {
+    let path = Path::new("/home/monal/git/adapton.rust/src/res.csv");
+    let mut writer = csv::Writer::from_file(path).unwrap();
+    for r in runtimes.into_iter() {
+      println!("{:?}",r);
+      writer.encode(r).ok().expect("CSV writer error");
+    }
+  };
+
+  init_naive();
+  assert!(engine_is_naive());
+  for i in 1..max_len {
+    let input = construct_input(i);
+    let naive_start = time::precise_time_ns();
+    let naive_out = doit(input);
+    let naive_end = time::precise_time_ns();
+    runtimes.push((i,naive_end - naive_start));}
+  csv_of_runtimes(runtimes.clone()); 
+  runtimes
+}
+
+#[test]
+fn test_runtime_harness() {
+  runtime_harness(10000);
 }
 
 pub fn generate_balanced_string () {
