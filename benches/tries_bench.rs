bcacd92de457258bc12c04e682042812470e8a5c
Sam Baxter <sbaxter@cs.umass.edu>
2016-12-18 12:32:00 -0500

    Add tree benchmarks for successful dcg speedup comparison

--- benches/tries_bench.rs
+++ benches/tries_bench.rs
@@ -2,6 +2,7 @@
 extern crate adapton;
 extern crate test;
 use self::test::Bencher;
+use adapton::collections::{Dir2, List, ListIntro, Tree, monoid_of_tree, tree_of_list};
 use adapton::trie::*;
 use adapton::engine::*;
 use std::rc::Rc;
@@ -154,6 +155,45 @@ mod sum_fold {
         for i in (1..100).into_iter() {
             dcg_input = push_input(i, dcg_input);
             b.iter(|| doit(dcg_input.clone()))
+        }
+    }
+}
+
+mod tree_benchmarks {
+    use super::*;
+
+    fn sum_tree(l:List<usize>) -> usize {
+        let t = ns(name_of_str("tree_of_list"),
+                   ||tree_of_list::<_,_,Tree<_>,_>(Dir2::Left, l));
+        ns(name_of_str("monoid_of_tree"),
+           || monoid_of_tree(t, 0, Rc::new(|x, y| x + y)))
+    }
+
+    fn push_list(i: usize, l: List<usize>) -> List<usize> {
+        let l = List::art(cell(name_of_usize(i), l));
+        let l = List::name(name_of_usize(i), l);
+        List::cons(i, l)
+    }
+
+    #[bench]
+    fn benchmark_naive_tree(b: &mut Bencher) {
+        init_naive();
+        let mut naive_input: List<usize> = List::nil();
+
+        for i in (1..100).into_iter() {
+            naive_input = push_list(i, naive_input);
+            b.iter(|| sum_tree(naive_input.clone()))
+        }
+    }
+
+    #[bench]
+    fn benchmark_dcg_tree(b: &mut Bencher) {
+        init_dcg();
+        let mut dcg_input: List<usize> = List::nil();
+
+        for i in (1..100).into_iter() {
+            dcg_input = push_list(i, dcg_input);
+            b.iter(|| sum_tree(dcg_input.clone()))
         }
     }
 }
