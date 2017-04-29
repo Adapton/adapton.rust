306eadf77d468d2499b20d2829559cf6fefc0e28
Matthew Hammer <matthew.hammer@colorado.edu>
2017-04-29 08:38:31 -0600

    fix: forgot to add catalog/mod.rs

---
+++ src/catalog/mod.rs
@@ -0,0 +1,16 @@
+//! Experimental collections, including those for sequences and sets.
+//! (See also: [The IODyn crate](https://github.com/cuplv/iodyn.rust))
+//!
+//! These experiments have informed futher development, and currently
+//! provide regression tests for the Adapton engine in this crate.
+//! 
+//! **Important note**: The latest development of Adapton collections
+//! has moved: 
+//!
+//! - [The IODyn crate](https://github.com/cuplv/iodyn.rust)
+//! - [Benchmark results](https://github.com/cuplv/adapton-talk/#benchmark-results)
+//!
+
+pub mod collections ;
+pub mod bitstring ;
+mod trie ;
